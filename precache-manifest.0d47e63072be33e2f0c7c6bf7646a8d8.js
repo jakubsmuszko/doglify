2bfb013d53a37bff22dd16cb1decc1cef3ba1c08
Jakub <jakubsmuszko@gmail.com>
2020-03-06 12:41:34 +0100

    Updates

---
+++ precache-manifest.0d47e63072be33e2f0c7c6bf7646a8d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6426b48ee7b3cf1f0d0418126002d42a",
+    "url": "/doglify/index.html"
+  },
+  {
+    "revision": "7e6e18c33892520b14be",
+    "url": "/doglify/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a6f76da98a00dacfdd46",
+    "url": "/doglify/static/css/main.3975f633.chunk.css"
+  },
+  {
+    "revision": "7e6e18c33892520b14be",
+    "url": "/doglify/static/js/2.32715b0d.chunk.js"
+  },
+  {
+    "revision": "a6f76da98a00dacfdd46",
+    "url": "/doglify/static/js/main.e4044d61.chunk.js"
+  },
+  {
+    "revision": "4be391892b9de75793fd",
+    "url": "/doglify/static/js/runtime-main.c0ee33cc.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/doglify/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "d9b6ba595b059fc5d48e8f52c30f73b3",
+    "url": "/doglify/static/media/Montserrat-BlackItalic.d9b6ba59.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/doglify/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "88395bfe49c88b19f20eb5621ac97527",
+    "url": "/doglify/static/media/borys5.88395bfe.png"
+  },
+  {
+    "revision": "d665c382b207437dcb9a0f020af93eac",
+    "url": "/doglify/static/media/doglify-logo.d665c382.png"
+  }
+]);
